99bbbf40a1992305ae80832ddaa91a0a6155db5a
nmkhoi3006 <khoi30062005@gmail.com>
2024-11-06 20:19:47 +0700

    fix val

--- TrainStep.py
+++ TrainStep.py
@@ -36,17 +36,17 @@ def train():
     num_iter = int(len(data_train)/32)
     for epoch in range(1, num_epochs+1):
         #TRAIN
-        progress_bar = tqdm(dataloader_train, colour="blue")
-        print("TRAIN")
-        model.train()
-        for iter, (img, label) in enumerate(progress_bar):
-            prediction = model(img)
-            loss = criterion(prediction, label)
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            progress_bar.set_description(f"Epoch: {epoch}/{num_epochs} | Loss: {loss:.4f}")
+        # progress_bar = tqdm(dataloader_train, colour="blue")
+        # print("TRAIN")
+        # model.train()
+        # for iter, (img, label) in enumerate(progress_bar):
+        #     prediction = model(img)
+        #     loss = criterion(prediction, label)
+        #
+        #     optimizer.zero_grad()
+        #     loss.backward()
+        #     optimizer.step()
+        #     progress_bar.set_description(f"Epoch: {epoch}/{num_epochs} | Loss: {loss:.4f}")
 
         #VAL
         all_losses = []
@@ -61,12 +61,12 @@ def train():
                 all_predictions.extend(index.tolist())
                 all_labels.extend(label.tolist())
 
-                loss = criterion(index, label)
+                loss = criterion(prediction, label)
                 all_losses.append(loss)
 
         acc = accuracy_score(all_labels, all_predictions)
         loss = sum(all_losses)/len(all_losses)
-        print(f"Accuracy_Validation: {acc} | Loss_Validation{loss}")
+        print(f"Accuracy_Validation: {acc} | Loss_Validation: {loss}")
 
 
 
