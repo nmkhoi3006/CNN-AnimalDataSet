c3e2e33c886da61de0802f16f73773b1e831b8e8
nmkhoi3006 <khoi30062005@gmail.com>
2024-11-06 20:00:00 +0700

    val

--- TrainStep.py
+++ TrainStep.py
@@ -4,6 +4,8 @@ import torch
 from torch.utils.data import DataLoader
 import torch.nn as nn
 from torchvision.transforms import Compose, ToTensor, Resize
+from sklearn.metrics import accuracy_score
+from tqdm.autonotebook import tqdm
 
 def train():
     transforms = Compose([
@@ -17,13 +19,13 @@ def train():
     dataloader_train = DataLoader(
         dataset=data_train,
         batch_size=32,
-        num_workers=4,
+        num_workers=2,
         drop_last=False
     )
     dataloader_test = DataLoader(
         dataset=data_test,
         batch_size=32,
-        num_workers=4,
+        num_workers=2,
         drop_last=False
     )
 
@@ -33,17 +35,38 @@ def train():
     num_epochs = 100
     num_iter = int(len(data_train)/32)
     for epoch in range(1, num_epochs+1):
+        #TRAIN
+        progress_bar = tqdm(dataloader_train, colour="blue")
+        print("TRAIN")
         model.train()
-        for iter, (img, label) in enumerate(dataloader_train):
+        for iter, (img, label) in enumerate(progress_bar):
             prediction = model(img)
             loss = criterion(prediction, label)
 
-            print(f"Iter: {iter}/{num_iter} | Loss: {loss}")
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            progress_bar.set_description(f"Epoch: {epoch}/{num_epochs} | Loss: {loss:.4f}")
 
+        #VAL
+        all_losses = []
+        all_labels = []
+        all_predictions = []
+        print("VALIDATION")
+        model.eval()
+        with torch.inference_mode():
+            for img, label in (dataloader_test):
+                prediction = model(img)
+                _, index = torch.max(prediction, dim=1)
+                all_predictions.extend(index.tolist())
+                all_labels.extend(label.tolist())
 
+                loss = criterion(index, label)
+                all_losses.append(loss)
+
+        acc = accuracy_score(all_labels, all_predictions)
+        loss = sum(all_losses)/len(all_losses)
+        print(f"Accuracy_Validation: {acc} | Loss_Validation{loss}")
 
 
 
